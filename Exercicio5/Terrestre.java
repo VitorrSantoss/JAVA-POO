69617aeeec3c1fbd7ca85fa89d11bec0e7e33a0b
VitorrSantoss <166569430+VitorrSantoss@users.noreply.github.com>
2025-03-19 16:28:10 -0300

    Add files via upload

---
+++ Exercicio5/Terrestre.java
@@ -0,0 +1,39 @@
+package Exercicio5;
+
+public class Terrestre extends MeioDeTransporte{
+
+    // ATRIBUTOS
+    private int qtdRoda;
+    private double potencia;
+
+    // CONSTRUTORES
+    public Terrestre() { }
+
+    public Terrestre(int id, short ano, String modelo, double carga_maxima, int qtdRoda, double potencia) {
+        super(id, ano, modelo, carga_maxima);
+        this.qtdRoda = qtdRoda;
+        this.potencia = potencia;
+    }
+
+    // GETTER'S AND SETTER'S
+    public int getQtdRoda() {
+        return qtdRoda;
+    }
+
+    public void setQtdRoda(int qtdRoda) {
+        this.qtdRoda = qtdRoda;
+    }
+
+    public double getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(double potencia) {
+        this.potencia = potencia;
+    }
+
+    @Override
+    public void consumo(){
+        System.out.printf("Consumo: %s", potencia * getCarga_maxima() * 100);
+    }
+}
