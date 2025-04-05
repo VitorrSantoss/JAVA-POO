776e5c4fc0aeb632f7d974e37264e846554e647f
VitorrSantoss <166569430+VitorrSantoss@users.noreply.github.com>
2025-04-05 20:27:43 -0300

    Delete Exercicio12 directory

--- Exercicio12/Canine.java
+++
@@ -1,6 +0,0 @@
-package Exercicio12;
-
-public class Canine extends Animal{
-
-    public void vaguear(){}
-}
--- Exercicio12/Feline.java
+++
@@ -1,6 +0,0 @@
-package Exercicio12;
-
-public class Feline extends Animal{
-
-    public void vaguear(){}
-}
